7dedcb30b50ce1f2062f1082b3d81cc995b99472
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-02-11 20:23:00 +0530

    new ques

--- practice ques/ques3_part2.c
+++ practice ques/ques3_part2.c
@@ -1,2 +1,14 @@
 // first part- swap using 3 var, b) without using third variable
 #include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("%d %d", a, b);
+    return 0;
+}
