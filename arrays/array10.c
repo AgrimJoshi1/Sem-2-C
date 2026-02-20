4885546c195d6dc5f1e5cc5e95ce781f33128356
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-02-20 21:35:14 +0530

    ques completed

--- arrays/array10.c
+++ arrays/array10.c
@@ -1,1 +1,35 @@
 //wap to remove duplicate elements from array
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                for(int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--;      
+                j--;      
+            }
+        }
+    }
+
+    printf("Array after removing duplicates:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
